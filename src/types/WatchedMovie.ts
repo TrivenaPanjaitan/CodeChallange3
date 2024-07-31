599068cdd6121413d5a457699cb2934e4b47c091
TrivenaPanjaitan <trivena.panjaitan@ist.id>
2024-08-01 02:25:36 +0700

    fixing

--- src/types/WatchedMovie.ts
+++ src/types/WatchedMovie.ts
@@ -2,7 +2,7 @@
 export interface WatchedMovie {
   id: number;
   title: string;
-  release_date: string; // or another date format if you prefer
-  poster_path: string; // assuming you have a URL to the poster image
-  // Add any other properties that are relevant
+  release_date: string;
+  poster_path: string;
+  // Other properties
 }
