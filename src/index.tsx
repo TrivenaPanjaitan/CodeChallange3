4c28a30be3753e71d1cd184614d0d80ffdb05fad
TrivenaPanjaitan <trivena.panjaitan@ist.id>
2024-08-02 00:25:15 +0700

    fixing

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { getBaseUrl } from './helpers/getBaseUrl';
 
-const basename = getBaseUrl() || '/';
+const basename = getBaseUrl();
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
