dbf0159780b3528316da747d05f3a9059454ea80
TrivenaPanjaitan <trivena.panjaitan@ist.id>
2024-08-01 23:39:08 +0700

    fixing

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,19 +3,21 @@ import { Link } from 'react-router-dom';
 import styles from './index.module.scss';
 
 const Header: React.FC = () => {
+  const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`; 
+
   return (
     <header className={styles['header']}>
       <h1>Movie App</h1>
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to={`${process.env.PUBLIC_URL}/`}>Home</Link>
           </li>
           <li>
-            <Link to="/watchlist">Watchlist</Link>
+            <Link to={`${process.env.PUBLIC_URL}/watchlist`}>Watchlist</Link>
           </li>
           <li>
-            <Link to="/rated">Rated Movies</Link>
+            <Link to={`${process.env.PUBLIC_URL}/rated`}>Rated Movies</Link>
           </li>
         </ul>
       </nav>
