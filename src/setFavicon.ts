83f6e0f35bfcfecac214cc00fb65be47950d84e0
TrivenaPanjaitan <trivena.panjaitan@ist.id>
2024-08-02 02:19:24 +0700

    fixing

--- src/setFavicon.ts
+++ src/setFavicon.ts
@@ -1,10 +1,8 @@
-// src/setFavicon.ts
 const setFavicon = (path: string) => {
-    const link = document.createElement('link');
-    link.rel = 'icon';
-    link.href = path;
-    document.head.appendChild(link);
-  };
-  
-  export default setFavicon;
-  
+  const link = document.createElement('link');
+  link.rel = 'icon';
+  link.href = `${process.env.PUBLIC_URL || ''}/${path}`;
+  document.head.appendChild(link);
+};
+
+export default setFavicon;
