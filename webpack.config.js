9868f7c5646473a4e4c3b2e4345e8721e7136061
TrivenaPanjaitan <trivena.panjaitan@ist.id>
2024-08-02 00:45:04 +0700

    fixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,82 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const { GenerateSW } = require("workbox-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require("webpack");
-const path = require("path");
-const dotenv = require("dotenv");
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
+const dotenv = require('dotenv');
 
 // Load environment variables
 dotenv.config();
 
-const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === 'production';
 
-const stylesHandler = isProduction
-  ? MiniCssExtractPlugin.loader
-  : "style-loader";
+// Generate environment variables for DefinePlugin
+const envKeys = Object.keys(process.env).reduce((prev, key) => {
+  prev[`process.env.${key}`] = JSON.stringify(process.env[key]);
+  return prev;
+}, {});
+
+const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader';
 
 module.exports = {
-  mode: process.env.NODE_ENV || "development",
-  entry: "./src/index.tsx",
+  mode: isProduction ? 'production' : 'development',
+  entry: './src/index.tsx',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "build"),
-    publicPath: isProduction ? "/CodeChallange3/" : "/",
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: isProduction ? '/CodeChallange3/' : '/',
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js", ".json", ".scss", ".ttf", ".webp", ".png", ".svg"],
+    extensions: ['.tsx', '.ts', '.js', '.json', '.scss', '.ttf', '.webp', '.png', '.svg'],
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
+        loader: 'ts-loader',
         exclude: /node_modules/,
-        include: path.resolve(__dirname, "src"),
+        include: path.resolve(__dirname, 'src'),
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader", "sass-loader"],
-        include: path.resolve(__dirname, "src"),
+        use: [stylesHandler, 'css-loader', 'postcss-loader', 'sass-loader'],
+        include: path.resolve(__dirname, 'src'),
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader", "postcss-loader"],
-        include: path.resolve(__dirname, "src"),
+        use: [stylesHandler, 'css-loader', 'postcss-loader'],
+        include: path.resolve(__dirname, 'src'),
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif|webp)$/i,
-        type: "asset",
-        include: path.resolve(__dirname, "src"),
+        type: 'asset',
+        include: path.resolve(__dirname, 'src'),
       },
     ],
   },
-  devtool: "source-map",
+  devtool: isProduction ? 'source-map' : 'inline-source-map',
   devServer: {
-    static: path.join(__dirname, "build"),
+    static: path.join(__dirname, 'build'),
     compress: true,
     port: 9000,
     historyApiFallback: true,
+    hot: !isProduction,
+    open: !isProduction,
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': JSON.stringify(process.env),
-    }),
+    new webpack.DefinePlugin(envKeys),
     new HtmlWebpackPlugin({
-      template: "./public/index.html",
+      template: './public/index.html',
     }),
-    ...(isProduction ? [] : [new BundleAnalyzerPlugin()]),
-    new MiniCssExtractPlugin({
-      filename: "styles.css",
-    }),
+    ...(isProduction ? [new MiniCssExtractPlugin({ filename: 'styles.css' })] : []),
     ...(isProduction
       ? [
           new GenerateSW({
             exclude: [/\.map$/, /asset-manifest\.json$/],
             runtimeCaching: [
               {
-                urlPattern: new RegExp("^https://.*"),
-                handler: "NetworkFirst",
+                urlPattern: new RegExp('^https://.*'),
+                handler: 'NetworkFirst',
               },
             ],
             skipWaiting: true,
@@ -85,5 +85,6 @@ module.exports = {
           }),
         ]
       : []),
+    ...(isProduction ? [] : [new webpack.HotModuleReplacementPlugin()]),
   ],
 };
